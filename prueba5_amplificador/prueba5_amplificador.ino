1b3b18cbc48d7fc2d5d67de8073497a74b2e913e
aracelinoguera <araceli.noguera@upa.edu.py>
2024-11-12 19:34:33 -0300

    prueba 5

--- prueba5_amplificador/prueba5_amplificador.ino
+++ prueba5_amplificador/prueba5_amplificador.ino
@@ -1,8 +1,8 @@
-// Conexión del ESP32 con el Amplificador de celda de carga HX711
+// Etapa 5: Conexión del ESP32 con el Amplificador de celda de carga HX711
 
 #include "HX711.h"
 
-// HX711 circuit wiring
+// Definir pines del HX711
 const int LOADCELL_DOUT_PIN = 18;
 const int LOADCELL_SCK_PIN = 19;
 
@@ -17,10 +17,10 @@ void loop() {
 
   if (scale.is_ready()) {
     long reading = scale.read();
-    Serial.print("HX711 reading: ");
+    Serial.print("Leyendo HX711: ");
     Serial.println(reading);
   } else {
-    Serial.println("HX711 not found.");
+    Serial.println("HX711 no encontrado.");
   }
 
   delay(1000);
